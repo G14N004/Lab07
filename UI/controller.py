7836dfc688ae9be0db836e70598ce3ef21e4ab59
G14N004 <s325606@studenti.polito.it>
2026-04-29 10:57:00 +0200

    versione1

--- UI/controller.py
+++ UI/controller.py
@@ -2,6 +2,7 @@ import flet as ft
 
 from UI.view import View
 from model.model import Model
+from model.situazione import Situazione
 
 
 class Controller:
@@ -14,12 +15,52 @@ class Controller:
         self._mese = 0
 
     def handle_umidita_media(self, e):
-        pass
+        lista = self._model.getAllSituazioni()
+        match= self._mese
+        dizionario={}
+        for elm in lista :
+            if isinstance(elm,Situazione):
+                mese = int(elm.data.month)
+                if mese == match:
+                    if elm.localita not in dizionario:
+                        dizionario[elm.localita]=[]
+
+                    dizionario[elm.localita].append(elm.umidita)
+
+
+        self._view.lst_result.controls.clear()
+        self._view.lst_result.controls.append(ft.Text("L'umidità media nel mese selezionato è : "))
+
+        dizionario_media={}
+        for chiave,valore in dizionario.items():
+            media = sum(valore)/len(valore)
+            dizionario_media[chiave]=media
+            self._view.lst_result.controls.append(ft.Text(f"{chiave} : {dizionario_media[chiave]:.4f}"))
+
+        self._view.update_page()
+
+
+
+
+
+
 
 
 
     def handle_sequenza(self, e):
-        pass
+        lista=self._model.getAllSituazioni()
+        match= self._mese
+        lista_filtrata=[elm for elm in lista if elm.data.month == match]
+        lista_finale=[elm for elm in lista_filtrata if 1<=elm.data.day<=15]
+        self._view.lst_result.controls.clear()
+
+
+
+        self._view.update_page()
+
+
+
+
 
     def read_mese(self, e):
         self._mese = int(e.control.value)
